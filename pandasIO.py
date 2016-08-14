97ff70bac396f54fe679547914bd470f98b03121
ariel-iporre-coderoad-com <ariel.iporre.rivas@gmail.com>
2016-08-14 00:51:41 -0400

    managing data with io tools

---
+++ pandasIO.py
@@ -0,0 +1,56 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug 13 19:40:22 2016
+
+Pandas IO
+
+Basics excersices practicing pandas
+
+Pandas help to manage information in same way no matters its format, that means
+that you can use the same commands and literary write the same code for 
+datasets completly different. You may use csv, xml or many other formats 
+as outputs or inputs, which means that you can have for example a csv input
+and a xml output or the same way around. 
+
+@author: aiporre
+"""
+
+import pandas as pd
+
+df =  pd.read_csv('BCB-3105.csv')
+print df.head()
+
+df.set_index('Date', inplace = True)
+df.to_csv('newcsv2.csv')
+df = pd.read_csv('newcsv2.csv')
+print df.head()
+
+#you can specify the index when you read it
+df = pd.read_csv('newcsv2.csv', index_col=0)
+print df.head()
+
+# you can rename colums every single column
+df.columns = ['BoBraExport']
+print df.head()
+df.to_csv('newcsv3.csv')# save to a csv fie
+
+# may you don want the name in the file so 
+# pandas can manage this
+
+df.to_csv('newcsv4.csv',header = False)# save to a csv fie
+
+# we can do all the above in one command line
+df = pd.read_csv('newcsv4.csv',names = ['Date','Bolivia exports to Brazil'], index_col=0)# save to a csv fie
+print df.head() # doesn't affect the data
+
+#######
+# converting to other formats
+df.to_html('bobraexport.html')
+
+df = pd.read_csv('newcsv4.csv', names=['Date','Exports'],index_col=0)
+print df.head()
+
+
+
+
+
