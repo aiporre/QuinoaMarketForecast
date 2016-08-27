37f88d306ae310767cdd58bd598fbede14e7ca00
ariel-iporre-coderoad-com <ariel.iporre.rivas@gmail.com>
2016-08-27 17:15:20 -0400

    Visualization of bolivian exports to brazil

--- pandasIO.py
+++ pandasIO.py
@@ -16,39 +16,52 @@ and a xml output or the same way around.
 """
 
 import pandas as pd
+import matplotlib.pyplot as plt
 
-df =  pd.read_csv('BCB-3105.csv')
+
+df =  pd.read_csv('data/BCB-3105.csv')
 print df.head()
 
 df.set_index('Date', inplace = True)
-df.to_csv('newcsv2.csv')
-df = pd.read_csv('newcsv2.csv')
+df.to_csv('data/newcsv2.csv')
+df = pd.read_csv('data/newcsv2.csv')
 print df.head()
 
 #you can specify the index when you read it
-df = pd.read_csv('newcsv2.csv', index_col=0)
+df = pd.read_csv('data/newcsv2.csv', index_col=0)
 print df.head()
 
 # you can rename colums every single column
 df.columns = ['BoBraExport']
 print df.head()
-df.to_csv('newcsv3.csv')# save to a csv fie
+df.to_csv('data/newcsv3.csv')# save to a csv fie
 
 # may you don want the name in the file so 
 # pandas can manage this
 
-df.to_csv('newcsv4.csv',header = False)# save to a csv fie
+df.to_csv('data/newcsv4.csv',header = False)# save to a csv fie
 
 # we can do all the above in one command line
-df = pd.read_csv('newcsv4.csv',names = ['Date','Bolivia exports to Brazil'], index_col=0)# save to a csv fie
+df = pd.read_csv('data/newcsv4.csv',names = ['Date','Bolivia exports to Brazil'], index_col=0)# save to a csv fie
 print df.head() # doesn't affect the data
 
 #######
 # converting to other formats
-df.to_html('bobraexport.html')
+df.to_html('data/bobraexport.html')
 
-df = pd.read_csv('newcsv4.csv', names=['Date','Exports'],index_col=0)
+df = pd.read_csv('data/newcsv4.csv', names=['Date','Exports'],index_col=0)
 print df.head()
+
+
+###########
+# visualization
+ts = df.cumsum()
+print "Plotting " + str(ts)
+ts.plot()
+plt.title("Exports from Bolivia to Brazil")
+plt.xlabel("date (End of month)")
+plt.ylabel("Amount of exports")
+plt.show()
 
 
 
